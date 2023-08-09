915bf8d9732a5c68ad96f714466b081530fc31bc
Chun Ho <chunhochoi2000@gmail.com>
2023-08-09 16:21:39 -0400

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -10,7 +10,7 @@ loginButton.addEventListener("click", (e) => {
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in.");
         setTimeout(function(){
-            window.location.replace(home.html)
+            window.location.replace(/home.html)
         },1000);
 
     } else {
