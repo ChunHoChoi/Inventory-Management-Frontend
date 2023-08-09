99eec4c0621135ec53e9592c1d7012e43f1ddc1d
Chun Ho <chunhochoi2000@gmail.com>
2023-08-09 19:39:35 +0000

    add home page and update login

--- functions/login.js
+++ functions/login.js
@@ -9,6 +9,25 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in.");
+        setTimeout(function(){
+            window.location.replace(home.html)
+        },1000);
+
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
         location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
